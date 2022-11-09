7cd065f1fb6950ef1cd23ec2af4d7403875cf8f2
pikezeed <pikezeed@users.noreply.github.com>
2022-11-09 15:27:23 +0700

    2 files modified

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function index() {
+  return (
+    <>
+      <h1 classname="title">
+        Read <Link href="/posts/first-post">this page!</Link>
+      </h1>
+    </>
+  );
+}
